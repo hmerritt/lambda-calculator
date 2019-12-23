039c13e2d23e604bdea4db72c70263f78997a9e4
Harry Merritt <harry@merritt.es>
2019-12-23 04:04:24 +0000

    Improve styleing

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useState } from "react";
 import "./App.css";
 // STEP 4 - import the button and display components
 // Don't forget to import any extra css/scss files you build into the correct component
@@ -17,13 +17,16 @@ function App() {
     // the "5" button, or the operator if they click one of those buttons) and then call your setter function to update state.
     // Don't forget to pass the functions (and any additional data needed) to the components as props
 
+    //  Final display value
+    const [displayValue, setDisplayValue] = useState(0);
+
     return (
         <div className="container">
             <div className="calculator">
                 <Logo />
                 <div className="App">
                     {/* STEP 4 - Render your components here and be sure to properly import/export all files */}
-                    <Display />
+                    <Display value={displayValue} />
                     <div className="buttons">
                         <div className="left">
                             <Specials />
